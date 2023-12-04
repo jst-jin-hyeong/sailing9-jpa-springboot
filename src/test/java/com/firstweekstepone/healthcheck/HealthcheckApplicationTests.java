e8d5ad7a45c019a215537ae9ff0711a3ce583459
jst-jin-hyeong <daramjw@naver.com>
2023-12-05 00:05:27 +0900

    0.0.0-200: nano phase - basic project and actuator

---
+++ src/test/java/com/firstweekstepone/healthcheck/HealthcheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstweekstepone.healthcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
